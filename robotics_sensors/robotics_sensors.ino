3c8055804f34b61544faa33810d6f0f4b60bc848
Wahid Bawa <wahidbawa10@gmail.com>
2018-11-05 14:37:29 -0500

    more sensors

---
+++ robotics_sensors/robotics_sensors.ino
@@ -0,0 +1,21 @@
+#include <Ultrasonic.h>
+Ultrasonic sonic1(3);
+int red = 5;    
+int green = 6;
+void setup() {
+  Serial.begin(9600);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+}
+
+void loop() {
+  int distanceCm1 = sonic1.distanceRead();
+  if (distanceCm1 > 10) { 
+    analogWrite(red, 0);
+    analogWrite(green, 255); 
+    }else{
+      analogWrite(green, 0); 
+      analogWrite(red, 255);
+    }
+}
+
---
+++ ultra/ultra.ino
@@ -0,0 +1,12 @@
+#include <Ultrasonic.h>
+Ultrasonic sonic1(3, 5);
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int Dist1 = sonic1.distanceRead();
+  if (Dist1 > 0){
+    Serial.println(Dist1);
+  }
+}
